4e1452fdeabb0314abd8457c639f6505b731b89e
Mayank Nader <mayank.nader11@gmail.com>
2019-02-11 01:29:31 +0530

    fix

--- app/routes.py
+++ app/routes.py
@@ -302,7 +302,7 @@ class Listing(Resource):
                                             "quantity": listing.quantity, "expiry": listing.expiry, "description": listing.description,
                                             "type": listing.type, "image": listing.image, "donor_id": listing.donor_id, "street": address.street,
                                             "landmark": address.landmark, "city": address.city, "country": address.country, 'organisation': donor.organisation,
-                                            "module": listing.module, "lat": listing.lat, "lng": listing.lng}
+                                            "module": listing.module, "lat": int(listing.lat), "lng": int(listing.lng)}
             return listing_dict
         else:
             return {"message": "send_all not given"}, 400
